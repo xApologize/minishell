2fbe092012a7c9fd72353cdd3f2e8564990f11f5
tastycool <tombl@live.ca>
2022-06-21 14:10:06 -0400

    debuter le parsing

--- src/parsing.c
+++ src/parsing.c
@@ -6,9 +6,13 @@
 /*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 10:29:20 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/21 10:43:14 by tberube-         ###   ########.fr       */
+/*   Updated: 2022/06/21 13:45:08 by tberube-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
 
+void	parsing(int argc, char **argv, char **envp)
+{
+	check_infile
+}
