9c428d0e08855a9f1c010238a12abe5885ef3103
Thomas <tombl@live.ca>
2022-06-21 15:33:39 -0400

    inisialiser la struct

--- src/minishell.c
+++ src/minishell.c
@@ -1,18 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 09:55:28 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/21 10:43:11 by tberube-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "include/minishell.h"
 
 int	main(int argc, char **argv, char **envp)
 {
-	parsing(argc, argv, envp);
+	t_data data;
+	parsing(argc, argv, envp, &data);
 }
