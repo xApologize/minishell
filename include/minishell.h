b611e0a346ae08984cfdd0e2d790fcfbf5546b90
tastycool <tombl@live.ca>
2022-06-21 10:48:28 -0400

    debuter le header

--- include/minishell.h
+++ include/minishell.h
@@ -6,10 +6,20 @@
 /*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 10:37:58 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/21 10:38:41 by tberube-         ###   ########.fr       */
+/*   Updated: 2022/06/21 10:47:30 by tberube-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdbool.h>
+# include <stdio.h>
+# include "libft/include/libft.h"
+# include <limits.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+
+#endif
