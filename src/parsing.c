eb970f6b9a766d5032d8313441bf6bd88599d86d
Thomas <tombl@live.ca>
2022-06-21 15:34:33 -0400

    spliter le path

--- src/parsing.c
+++ src/parsing.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 10:29:20 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/21 13:45:08 by tberube-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "include/minishell.h"
 
-void	parsing(int argc, char **argv, char **envp)
+void	parsing(int argc, char **argv, char **envp, t_data *data)
 {
-	check_infile
+	search_split_path(envp, data);
 }
