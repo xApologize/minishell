ab9279b7aa221aabca3a0ee9ee6cad344fdaae84
tastycool <tombl@live.ca>
2022-06-23 13:27:15 -0400

    cherche les quotes

--- src/quotation.c
+++ src/quotation.c
@@ -1,3 +1,31 @@
-#include "include/minishell.h"
+#include "../include/minishell.h"
 
-void	error_quotation()
+void	error_quotation(t_data *data)
+{
+	int		i;
+	bool	pair;
+
+	i = 0;
+	pair = true;
+	while (data->argvs_join[i] != '\0')
+	{
+		if (data->argvs_join[i] == '\'')
+		{
+			if (pair == true)
+				pair = false;
+			else
+				pair = true;
+			i++;
+		}
+		i++;
+	}
+	if (pair == false)
+		search_last_quote(data, i);
+}
+
+void	search_last_quote(t_data *data, int i)
+{
+	while (data->argvs_join[i] != '\'')
+		i--;
+	
+}
