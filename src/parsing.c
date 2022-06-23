0d9df901e01bbc5309c49d0919bb10ef6a4e4715
tastycool <tombl@live.ca>
2022-06-23 10:52:52 -0400

    joindre les argvs pour faire une string

--- src/parsing.c
+++ src/parsing.c
@@ -3,4 +3,17 @@
 void	parsing(int argc, char **argv, char **envp, t_data *data)
 {
 	search_split_path(envp, data);
+	join_all_argv(argv, data);
+}
+
+void	join_all_argv(char **argv, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i + 1] != NULL)
+	{
+		data->argvs_join = ft_strjoin(argv[i], argv[i + 1]);
+		i++;
+	}
 }
