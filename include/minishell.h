cd7a91e0cbc21cd50802d5de905e6f334152cf0a
tastycool <tombl@live.ca>
2022-06-23 10:51:56 -0400

    mettre les prototype et ajout de data->argvs_join

--- include/minishell.h
+++ include/minishell.h
@@ -11,8 +11,13 @@ typedef struct s_data
 {
 	char	*path;
 	char	**path_split;
+	char	*argvs_join;
 
 } t_data;
 
-
+int		main(int argc, char **argv, char **envp);
+void	parsing(int argc, char **argv, char **envp, t_data *data);
+void	join_all_argv(char **argv, t_data *data);
+void	search_split_path(char **envp, t_data *data);
+void	split_path(t_data *data);
 #endif
