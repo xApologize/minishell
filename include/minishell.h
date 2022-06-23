4a5c7da31e1258f1a14d486f09d4035768ac168b
fenix144 <yan_1731@hotmail.com>
2022-06-22 21:49:39 -0400

    Add readline/history header files

--- include/minishell.h
+++ include/minishell.h
@@ -6,10 +6,12 @@ typedef struct s_data
 
 }	t_data;
 
-# include "libft/include/libft.h"
+# include "../libft/include/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
