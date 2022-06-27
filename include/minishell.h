6ea5d5a0ac906221a16cb233637ae93407b576af
fenix144 <yan_1731@hotmail.com>
2022-06-27 15:30:28 -0400

    Add prototype of sigint_handler

--- include/minishell.h
+++ include/minishell.h
@@ -14,7 +14,12 @@ typedef struct s_data
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
+# include <string.h>
+# include "/opt/homebrew/Cellar/readline/8.1.2/include/readline/readline.h"
+
+// option de compil macos + homebrew: gcc minishell.c rl_gets.c  -lreadline -L /opt/homebrew/Cellar/readline/8.1.2/lib -I /opt/homebrew/Cellar//readline/8.1.2/include
 
 char	*rl_gets(void);
+void	sigint_handler(int signum);
 
 #endif
