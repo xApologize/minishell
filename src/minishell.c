47594746485a1d0e4265f7be4e88e86d19b71aa4
fenix144 <yan_1731@hotmail.com>
2022-06-27 16:48:21 -0400

    Add sigaction for sigquit. Sigquit is ignored

--- src/minishell.c
+++ src/minishell.c
@@ -14,11 +14,14 @@ int main(void)
 {
 	char	*line;
 	struct sigaction sa_sigint;
+	struct sigaction sa_sigquit;
 
-	sa_sigint.sa_handler = sigint_handler;
+	sa_sigint.sa_handler = sigint_handler; 
+	sa_sigquit.sa_handler = SIG_IGN;
 	sigemptyset(&sa_sigint.sa_mask);
 	sa_sigint.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_sigint, NULL);
+	sigaction(SIGQUIT, &sa_sigquit, NULL);
 	while (1)
 	{
 		line = rl_gets();
