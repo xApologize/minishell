9de6384b9137420654632c7316af026007c0964d
fenix144 <yan_1731@hotmail.com>
2022-06-27 16:53:24 -0400

    Move signal handling implementation to sig_handling.c

--- src/minishell.c
+++ src/minishell.c
@@ -13,15 +13,8 @@
 int main(void)
 {
 	char	*line;
-	struct sigaction sa_sigint;
-	struct sigaction sa_sigquit;
 
-	sa_sigint.sa_handler = sigint_handler; 
-	sa_sigquit.sa_handler = SIG_IGN;
-	sigemptyset(&sa_sigint.sa_mask);
-	sa_sigint.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa_sigint, NULL);
-	sigaction(SIGQUIT, &sa_sigquit, NULL);
+	sig_handling();
 	while (1)
 	{
 		line = rl_gets();
