843f3697f91bdc36ddc0da711e3dd07721f950c2
fenix144 <yan_1731@hotmail.com>
2022-06-27 15:29:09 -0400

    Add sigint_handler function

---
+++ src/sigint_handler.c
@@ -0,0 +1,12 @@
+#include "../include/minishell.h"
+
+void	sigint_handler(int signum)
+{
+	if (signum == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
