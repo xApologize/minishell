b647ea8cca0e577f6b132d8f61f21b77f0a3236b
fenix144 <yan_1731@hotmail.com>
2022-06-28 17:16:07 -0400

    Add function to handle export builtin

--- src/handle_export.c
+++ src/handle_export.c
@@ -1,6 +1,17 @@
 #include "../include/minishell.h"
 
+// readline, rl_clear_history, rl_on_new_line,
+// rl_replace_line, rl_redisplay, add_history,
+// printf, malloc, free, write, access, open, read,
+// close, fork, wait, waitpid, wait3, wait4, signal,
+// sigaction, sigemptyset, sigaddset, kill, exit,
+// getcwd, chdir, stat, lstat, fstat, unlink, execve,
+// dup, dup2, pipe, opendir, readdir, closedir,
+// strerror, perror, isatty, ttyname, ttyslot, ioctl,
+// getenv, tcsetattr, tcgetattr, tgetent, tgetflag,
+// tgetnum, tgetstr, tgoto, tputs
+
 void	handle_export(char **opt)
 {
-	
+	printf("%s\n", opt[0]);
 }
