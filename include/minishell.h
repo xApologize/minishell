8075a369143a629bd9024e8f560966872533a60e
fenix144 <yan_1731@hotmail.com>
2022-06-28 09:50:02 -0400

    Add prototypes for builtins

--- include/minishell.h
+++ include/minishell.h
@@ -15,12 +15,20 @@ typedef struct s_data
 # include <readline/history.h>
 # include <signal.h>
 # include <string.h>
-# include "/opt/homebrew/Cellar/readline/8.1.2/include/readline/readline.h"
+# include "readline/readline.h"
 
 // option de compil macos + homebrew: gcc minishell.c rl_gets.c  -lreadline -L /opt/homebrew/Cellar/readline/8.1.2/lib -I /opt/homebrew/Cellar//readline/8.1.2/include
 
 char	*rl_gets(void);
 void	sigint_handler(int signum);
 void	sig_handling(void);
+void	handle_builtin(char *line);
+void	handle_cd(char **opt);
+void	handle_echo(char **opt);
+void	handle_env(char **opt);
+void	handle_exit(char **opt);
+void	handle_export(char **opt);
+void	handle_pwd(char **opt);
+void	handle_unset(char **opt);
 
 #endif
