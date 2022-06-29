bee5c0e074275e044d371298100030f48f4eb960
fenix144 <yan_1731@hotmail.com>
2022-06-29 16:50:19 -0400

    Remove function definitions print_echo_with_n & print_echo_without_n

--- src/handle_echo.c
+++ src/handle_echo.c
@@ -1,52 +1,5 @@
 #include "../include/minishell.h"
 
-void	print_echo_with_n(char *line)
-{
-	int		i;
-	int		j;
-	char 	*str;
-	char 	*pstr;
-
-	i = 0;
-	j = ft_strlen(line);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	while (ft_isalpha(line[i]) == 1)
-		i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	i += 2;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	str = ft_substr(line, i, (j - i));
-	pstr = ft_strtrim(str, ' ');
-	free(str);
-	printf("%s", pstr);
-	free(pstr);
-}
-
-void	print_echo_without_n(char *line)
-{
-	int		i;
-	int		j;
-	char	*str;
-	char	*pstr;
-
-	i = 0;
-	j = ft_strlen(line);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	while (ft_isalpha(line[i]) == 1)
-		i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	str = ft_substr(line, i, (j - i));
-	pstr = ft_strtrim(str, ' ');
-	free(str);
-	printf("%s\n", pstr);
-	free(pstr);
-}
-
 void	handle_echo(char *line, char **opt)
 {
 	if (ft_strcmp(opt[1], "-n") == 0)
