2736fae4000fef0c95df8cd14e518c23f7b49377
xApologize <jenbenrossignol@hotmail.com>
2022-07-04 14:59:25 -0400

    add function for execve and loop for cmd

--- src/pipex.c
+++ src/pipex.c
@@ -3,18 +3,34 @@
 void	pipex(t_cmd *cmd)
 {
 	int	pid;
+	int	pipe_fd[2];
 
+	pipe(pipe_fd);
 	while (cmd != NULL)
 	{
 		pid = fork();
 		if (pid == 0)
-			exec_cmd(cmd);
+			exec_cmd(cmd, pipe_fd);
 		cmd = cmd->next;
 	}
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
 }
 
-void	exec_cmd(t_cmd *cmd)
+void	exec_cmd(t_cmd *cmd, int pipe_fd[2])
 {
-	printf("cmd-> cmd: %s\n", cmd->cmd);
+	if (cmd->first == 1)
+		dup2(pipe_fd[1], 1);
+	else if (cmd->first == 0 && cmd->next != NULL)
+	{
+		dup2(pipe_fd[0], 0);
+		dup2(pipe_fd[1], 1);
+	}
+	else 
+		dup2(pipe_fd[0], 0);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	execve(cmd->cmd, cmd->argv, cmd->envp);
+	dprintf(2, "something went wrong: %s\n", cmd->cmd);
 	exit(0);
 }
