49395709b48d8770a5561ff21e704f77d5987c10
fenix144 <yan_1731@hotmail.com>
2022-07-08 08:41:15 -0400

    Add return for envp_copy

--- src/handle_unset.c
+++ src/handle_unset.c
@@ -1,8 +1,9 @@
 #include "../include/minishell.h"
 
 // Unset without any options. Adding option triggers an error message. bash: unset: (option used): invalid option
-void	handle_unset(char **opt, char **envp_copy)
+char	**handle_unset(char **opt, char **envp_copy)
 {
 	access(envp_copy[0], F_OK);
 	printf("%s\n", opt[0]);	
+	return (NULL);
 }
