44cece6f3462fbb0b6f4e1e5e2dd9a936e9acf4c
fenix144 <yan_1731@hotmail.com>
2022-07-13 16:08:45 -0400

    Add call to sig_reset to restore default signal behaviors

--- src/parsing.c
+++ src/parsing.c
@@ -2,12 +2,14 @@
 
 void	parsing(char *line, char **envp_copy, t_data *data)
 {
+	sig_reset();
 	access(envp_copy[0], F_OK);
 	split_line(line, data);
 	env_split(data, envp_copy);
 	check_first(line, data);
 	search_cmd(data);
 	//error_quotation(data);
+	sig_handling();
 }
 
 void	split_line(char *line, t_data *data)
