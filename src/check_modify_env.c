18bcc2bf57b7c80f77f5040f6211cd9f5859f3b9
fenix144 <yan_1731@hotmail.com>
2022-07-15 13:07:38 -0400

    Add function to check if variable needs to be modified

--- src/check_modify_env.c
+++ src/check_modify_env.c
@@ -11,14 +11,18 @@ int	check_modify_env(char *arg, char **envp_copy)
 	while (envp_copy[++i])
 	{
 		split_envp_copy = ft_split(envp_copy[i], '=');
+		printf("%s\n", split_envp_copy[0]);
+		printf("%s\n", split_arg[0]);
 		if (ft_strcmp(split_arg[0], split_envp_copy[0]) == 0)
 		{
 			freeopt(split_arg);
 			freeopt(split_envp_copy);
+			printf("Returning 1\n");
 			return (1);
 		}
 		freeopt(split_envp_copy);
 	}
 	freeopt(split_arg);
+	printf("Returning 0\n");
 	return (0);
 }
