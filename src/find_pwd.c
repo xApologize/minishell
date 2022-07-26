a0ff61ccddeaf06c0e69fe52e84856f081176459
fenix144 <yan_1731@hotmail.com>
2022-07-26 12:48:15 -0400

    Add function to return index of pwd env variable

--- src/find_pwd.c
+++ src/find_pwd.c
@@ -2,5 +2,19 @@
 
 int	find_pwd(char **envp_copy)
 {
-	
+	int		index;
+	char	**envp_split;
+
+	index = -1;
+	while (envp_copy[++index])
+	{
+		envp_split = ft_split(envp_copy[index], '=');
+		if (ft_strcmp("PWD", envp_split[0]) == 0)
+		{
+			freeopt(envp_split);
+			return (index);
+		}
+		freeopt(envp_split);
+	}
+	return (-1);
 }
