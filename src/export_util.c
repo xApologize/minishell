b58b81eb64039e36abc05e23ba15757b4e0f6b22
fenix144 <yan_1731@hotmail.com>
2022-07-26 18:56:04 -0400

    Remove checkvalidenv duplicated declaration

--- src/export_util.c
+++ src/export_util.c
@@ -52,22 +52,6 @@ bool	checkvalidassign(bool env_stat, char *arg)
 	return (true);
 }
 
-bool	checkvalidenv(char *arg)
-{
-	int	i;
-
-	i = 0;
-	if (ft_isalpha(arg[0]) == 0)
-		return (false);
-	while (arg[i] != '=' && arg[i] != '\0')
-	{
-		if (ft_isalnum(arg[i]) == 0)
-			return (false);
-		i++;
-	}
-	return (true);
-}
-
 int	check_modify_env(char *arg, char **envp_copy)
 {
 	int		i;
