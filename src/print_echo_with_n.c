c75f89e156ff485fe84fe0fdf3967bc7968d90a7
fenix144 <yan_1731@hotmail.com>
2022-07-26 12:47:19 -0400

    Fix parsing for multiple -n flags

--- src/print_echo_with_n.c
+++ src/print_echo_with_n.c
@@ -1,5 +1,43 @@
 #include "../include/minishell.h"
 
+int	skip_echo(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i] == '\t' || line[i] == ' ')
+		i++;
+	while (ft_isalnum(line[i]) == 1)
+		i++;
+	return (i);
+}
+
+int	skip_n(char *line)
+{
+	int	i;
+	int	trigger;
+
+	i = skip_echo(line);
+	trigger = 0;
+	while (line[i])
+	{
+		if (line[i] == '-')
+			trigger = 1;
+		if (trigger == 1 && (line[i] == ' ' || line[i] == '\t'))
+			trigger = 0;
+		if (trigger == 0 && ft_isalnum(line[i]) == 1)
+			return (i); 
+		if (trigger == 1 && ft_isalnum(line[i]) == 1 && line[i] != 'n')
+		{
+			while (line[i] != ' ' && line[i] != '\t')
+				i--;
+			return (i);
+		}
+	i++;
+	}
+	return (-1);
+}
+
 void	print_echo_with_n(char *line)
 {
 	int		i;
@@ -7,19 +45,8 @@ void	print_echo_with_n(char *line)
 	char 	*str;
 	char 	*pstr;
 
-	i = 0;
+	i = skip_n(line);
 	j = ft_strlen(line);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	while (ft_isalpha(line[i]) == 1)
-		i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	i++;
-	while (ft_isalpha(line[i]) == 1)
-		i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
 	str = ft_substr(line, i, (j - i));
 	pstr = ft_strtrim(str, ' ');
 	free(str);
