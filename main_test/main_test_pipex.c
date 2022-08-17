9f37fce6db5962875f529cbe1cd75f42a355ca20
xApologize <jenbenrossignol@hotmail.com>
2022-08-17 11:34:56 -0400

    update main to test sleep

--- main_test/main_test_pipex.c
+++ main_test/main_test_pipex.c
@@ -12,31 +12,31 @@ int main()
 	cmd3->argv = calloc(sizeof(char *), 3);
 	cmd4->argv = calloc(sizeof(char *), 3);
 
-	cmd1->cmd = "/bin/sleep";
-	cmd1->argv[0] = "sleep";
-	cmd1->argv[1] = "10";
-
-	cmd2->cmd = "/bin/ls";
-	cmd2->argv[0] = "ls";
-	//cmd2->argv[1] = "1";
-
-	// cmd1->cmd = "/bin/cat";
-	// cmd1->argv[0] = "cat";
-	// cmd1->argv[1] = "main_test_pipex.c";
+	// cmd1->cmd = "/bin/ls";
+	// cmd1->argv[0] = "ls";
+	// cmd1->argv[1] = "-l";
  //
 	// cmd2->cmd = "/usr/bin/grep";
 	// cmd2->argv[0] = "grep";
-	// cmd2->argv[1] = "calloc";
- //
-	// cmd3->cmd = "/usr/bin/grep";
-	// cmd3->argv[0] = "grep";
-	// cmd3->argv[1] = "argv";
- //
-	// cmd4->cmd = "/usr/bin/wc";
-	// cmd4->argv[0] = "wc";
+	// cmd2->argv[1] = "add";
+
+	cmd1->cmd = "/bin/cat";
+	cmd1->argv[0] = "cat";
+	cmd1->argv[1] = "main_test_pipex.c";
+
+	cmd2->cmd = "/usr/bin/grep";
+	cmd2->argv[0] = "grep";
+	cmd2->argv[1] = "calloc";
+
+	cmd3->cmd = "/usr/bin/grep";
+	cmd3->argv[0] = "grep";
+	cmd3->argv[1] = "argv";
+
+	cmd4->cmd = "/usr/bin/wc";
+	cmd4->argv[0] = "wc";
 
 	cmd1->next = cmd2;
-	cmd2->next = NULL;
+	cmd2->next = cmd3;
 	cmd3->next = cmd4;
 	cmd4->next = NULL;
 	pipex(cmd1);
