7d16d0ab8c39cd25ce1385d11978e03801f46c79
xApologize <jenbenrossignol@hotmail.com>
2022-08-23 16:55:21 -0400

    add print value

--- src/file_redir.c
+++ src/file_redir.c
@@ -1,10 +1,46 @@
 #include "../include/minishell.h"
 
-void file_redir(char *line, t_data *data)
+int	get_redir(t_data *data)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	line = NULL;
-	data = NULL;
+	j = 0;
+	while (data->indexmeta[i])
+	{
+		if (ft_strchr("<>", data->indexmeta[i]))
+		{
+			if (data->indexmeta[i] == '>' && data->indexmeta[i + 1] == '>')
+			{
+				i++;
+				j++;
+			}
+			else
+				j++;
+		}
+		i++;
+	}
+	return (j);
+}
+
+void file_redir(char *line, t_data *data)
+{
+	int		i;
+	int		j;
+	char	**redir;
+
+	i = 0;
+	j = 0;
+	if (get_redir(data) > 0)
+		redir = ft_calloc(sizeof(char*), get_redir(data));
+	while (i < data->line_lenght)
+	{
+		if (line[i] == '\0')
+		{
+			if (data->indexmeta[j] == '>' || data->indexmeta[j] == '<')
+			{
+			}
+		}
+	}
 }
