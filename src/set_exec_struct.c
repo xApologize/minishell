4f6a37a6c56cbbc2b8cdbe8faa1cb2eef4c9799b
tastycool <tombl@live.ca>
2022-08-25 15:28:12 -0400

    commencer les list chainers

--- src/set_exec_struct.c
+++ src/set_exec_struct.c
@@ -1,64 +1,72 @@
 #include "../include/minishell.h"
 
-// void	set_exec_struct(char *line, t_cmd *cmd, t_data *data, char **env)
-// {
-// 	int		i;
-// 	int		noeud;
+void	set_exec_struct(char *line, t_cmd *cmd, t_data *data, char **env)
+{
+	int		i;
+	int		noeud;
 
-// 	i = 0;
-// 	cmd = NULL;
-// 	noeud = nb_pipes(data);
-// 	while (noeud > i++)
-// 	{
-// 		add_nodes(&cmd, create_nodes(cmd, argv, env));
-// 	}
-// 	while (i < data->line_lenght)
-// 	{
-// 		line[i];
-// 	}
-// }
+	i = 0;
+	(void)line;
+	noeud = nb_pipes(data);
+	while (i < noeud)
+	{
+		add_nodes(&cmd, create_nodes(env));
+		DEBUG;
+		i++;
+	}
+}
 
-// int	nb_pipes(t_data *data)
-// {
-// 	int	i;
-// 	int	j;
+int	nb_pipes(t_data *data)
+{
+	int	i;
+	int	j;
 
-// 	i = 0;
-// 	j = 0;
-// 	while (data->indexmeta[i] != '\0')
-// 	{
-// 		if (data->indexmeta[i] == '|')
-// 			j++;
-// 		i++;
-// 	}
-// 	return (j + 1);
-// }
+	i = 0;
+	j = 0;
+	while (data->indexmeta[i] != '\0')
+	{
+		if (data->indexmeta[i] == '|')
+			j++;
+		i++;
+	}
+	return (j + 1);
+}
 
-// t_cmd *create_nodes(char *cmd, char **argv, char **env)
-// {
-// 	t_cmd *node;
+t_cmd *create_nodes(char **env)
+{
+	t_cmd *node;
 
-// 	node = malloc(sizeof(t_cmd));
-// 	if (!node)
-// 		return(NULL);
-// 	node->cmd = cmd;
-// 	node->argv = argv;
-// 	node->environ = env;
-// 	node->next = NULL;
-// 	return (node);
-// }
+	node = malloc(sizeof(t_cmd));
+	if (!node)
+		return(NULL);
+	node->environ = env;
+	node->next = NULL;
+	return (node);
+}
 
-// void add_nodes(t_cmd **cmd, t_cmd *new_cmd)
-// {
-// 	t_cmd *last;
+void add_nodes(t_cmd **cmd, t_cmd *new_cmd)
+{
+	t_cmd *last;
 
-// 	if (cmd == NULL)
-// 		cmd = new_cmd;
-// 	else
-// 	{
-// 		last = ft_lstlast(*cmd);
-// 		last->next = new_cmd;
-// 	}
-// }
+	if (*cmd == NULL)
+		*cmd = new_cmd;
+	else
+	{
+		last = get_last(*cmd);
+		last->next = new_cmd;
+	}
+}
 
-// // malloc cmd = pipex split_path
+t_cmd *get_last(t_cmd *cmd)
+{
+	t_cmd *tmp;
+
+	tmp = cmd;
+	if (!cmd)
+		return(NULL);
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+// malloc cmd = pipex split_path
