0733127a65291f99194d56f2632be979e28ce951
xApologize <jenbenrossignol@hotmail.com>
2022-09-19 08:14:46 -0400

    remove print

--- src/status_error.c
+++ src/status_error.c
@@ -9,9 +9,9 @@ void	status(t_data *data)
 		dprintf(2, "quotes is odd\n");
 	if (data->error_first_cmd == true)
 	{
-		while (data->line[i] != '\0')
-			dprintf(2, "%c", data->line[i++]);
+		//while (data->line[i] != '\0')
+			//dprintf(2, "%c", data->line[i++]);
 		// regarder pour commande not found perror
-		printf("\n");
+		//printf("\n");
 	}
 }
