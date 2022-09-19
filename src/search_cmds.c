5979a943359a0a28d7d7d654d50dd190353fdf75
xApologize <jenbenrossignol@hotmail.com>
2022-09-19 11:37:27 -0400

    move functions to utils files

--- src/search_cmds.c
+++ src/search_cmds.c
@@ -49,21 +49,6 @@ int	set_cmd(t_cmd *cmd, t_data *data)
 	return (i - 1);
 }
 
-void	close_fd(t_cmd *cmd)
-{
-	t_cmd *tmp;
-
-	tmp = cmd;
-	while (tmp != NULL)
-	{
-		if (tmp->redir_in != STDIN_FILENO)
-			close(tmp->redir_in);
-		if (tmp->redir_out != STDOUT_FILENO)
-			close(tmp->redir_out);
-		tmp = tmp->next;
-	}
-}
-
 void	search_cmd(t_data *data, t_cmd *cmd)
 {
 	t_cmd	*tmp_cmd;
@@ -89,34 +74,4 @@ void	search_cmd(t_data *data, t_cmd *cmd)
 	}
 	pipex(cmd);
 	close_fd(cmd);
-}
-
-void	env_split(t_data *data, char **envp_copy)
-{
-	int	find;
-
-	find = 0;
-	data->path_split = NULL;
-	while (envp_copy[find])
-	{
-		if (ft_strncmp(envp_copy[find], "PATH=", 5) == 0)
-			break ;
-		find++;
-	}
-	if (envp_copy[find] != NULL)
-		data->path_split = ft_split(envp_copy[find], ':');
-	data->path_split[0] = data->path_split[0] + 5;
-}
-
-void	trim_path(t_data *data)
-{
-	char	*tmp;
-	int		i;
-
-	i = 0;
-	tmp = ft_strdup(data->path_split[0]);
-	data->path_split[0] = ft_substr(tmp, 5, ft_strlen(tmp));
-	free(tmp);
-	while (data->path_split[i])
-		i++;
 }
