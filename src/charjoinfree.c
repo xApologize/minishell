4215cc8263a67b1c9e6984429d631647ca975257
fenix144 <yan_1731@hotmail.com>
2022-09-23 19:52:10 -0400

    Fix dollar unwrapping

--- src/charjoinfree.c
+++ src/charjoinfree.c
@@ -6,12 +6,10 @@ char	*charjoinfree(const char *s1, const char c)
 	int		len;
 
 	len = ft_strlen(s1);
-	printf("len: %d\n", len);
 	newstring = ft_calloc(len + 2, sizeof(char));
 	len = 0;
 	while (s1[len])
 	{
-		write(1, &s1[len], 1);
 		newstring[len] = s1[len];
 		len++;
 	}
