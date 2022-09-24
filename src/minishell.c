9dc60f4abcb06bbaf6b2e28b2cd7bb4c9ea2c8b2
fenix144 <yan_1731@hotmail.com>
2022-09-23 21:03:59 -0400

    Add todo list in minishell.c

--- src/minishell.c
+++ src/minishell.c
@@ -1,16 +1,8 @@
 #include "../include/minishell.h"
 
-// readline, rl_clear_history, rl_on_new_line,
-// rl_replace_line, rl_redisplay, add_history,
-// printf, malloc, free, write, access, open, read,
-// close, fork, wait, waitpid, wait3, wait4, signal,
-// sigaction, sigemptyset, sigaddset, kill, exit,
-// getcwd, chdir, stat, lstat, fstat, unlink, execve,
-// dup, dup2, pipe, opendir, readdir, closedir,
-// strerror, perror, isatty, ttyname, ttyslot, ioctl,
-// getenv, tcsetattr, tcgetattr, tgetent, tgetflag,
-// tgetnum, tgetstr, tgoto, tputs
-
+// fix echo that prints quotes when it should not
+// need to deal with export that needs to work with variables between quotes
+// need to fix prompt that shows up twice when using ctrl-c when signal is reset
 int	main(void)
 {
 	t_data		data;
