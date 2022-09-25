413bd4909feae8c774dae4c2662cfdcbe408faaa
fenix144 <yan_1731@hotmail.com>
2022-09-25 17:55:38 -0400

    Fix improper error message when exit called

--- src/handle_exit.c
+++ src/handle_exit.c
@@ -5,16 +5,19 @@ void	handle_exit(t_cmd *cmd, t_data *data)
 	int exit_status;
 
 	exit_status = 0;
-	if (cmd->argv[2])
+	if (count_args(cmd) > 2)
 	{
 		dprintf(STDERR_FILENO, "minicougar: exit: too many arguments\n");
-		set_exit_code(-1);
+		set_exit_code(1);
 		return ;
 	}
-	if (cmd->argv[1] && check_if_num(cmd->argv[1]) == true)
-		exit_status = ft_atoi(cmd->argv[1]);
-	else
-		dprintf(STDERR_FILENO, "minicougar: exit: %s: numeric argument required\n", cmd->argv[1]);
+	if (count_args(cmd) == 2)
+	{
+		if (cmd->argv[1] && check_if_num(cmd->argv[1]) == true)
+			exit_status = ft_atoi(cmd->argv[1]);
+		else
+			dprintf(STDERR_FILENO, "minicougar: exit: %s: numeric argument required\n", cmd->argv[1]);
+	}
 	if (exit_status >= 0 && exit_status <= 127)
 		set_exit_code(exit_status);
 	else
@@ -36,4 +39,15 @@ bool	check_if_num(const char *n)
 			return (false);
 	}
 	return (true);
+}
+
+int	count_args(t_cmd *cmd)
+{
+	int	count;
+
+	count = -1;
+	while (cmd->argv[++count])
+	{
+	}
+	return (count);
 }
