88a7ae00cb125c6f591af7ddca162cd173df795d
fenix144 <yan_1731@hotmail.com>
2022-09-27 18:11:35 -0400

    Fix leaks with cd

--- src/cd_utils.c
+++ src/cd_utils.c
@@ -2,30 +2,30 @@
 
 //same as ft_strjoinfree. joins 2 strings and
 //frees the once given as first argument
-static char	*ft_pwdjoinfree(const char *s1, const char *s2)
-{
-	int		s1len;
-	int		s2len;
-	int		i;
-	char	*newstring;
+// static char	*ft_pwdjoinfree(const char *s1, const char *s2)
+// {
+// 	int		s1len;
+// 	int		s2len;
+// 	int		i;
+// 	char	*newstring;
 
-	if (!s1 || !s2)
-		return (NULL);
-	s1len = ft_strlen((char *)s1);
-	s2len = ft_strlen((char *)s2);
-	i = s1len;
-	newstring = ft_calloc((s1len + s2len + 1), sizeof(char));
-	if (!newstring)
-		return (NULL);
-	newstring = ft_memcpy(newstring, s1, s1len);
-	while (i < s1len + s2len)
-	{
-		newstring[i] = s2[i - s1len];
-		i++;
-	}
-	free((void *) s2);
-	return (newstring);
-}
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	s1len = ft_strlen((char *)s1);
+// 	s2len = ft_strlen((char *)s2);
+// 	i = s1len;
+// 	newstring = ft_calloc((s1len + s2len + 1), sizeof(char));
+// 	if (!newstring)
+// 		return (NULL);
+// 	newstring = ft_memcpy(newstring, s1, s1len);
+// 	while (i < s1len + s2len)
+// 	{
+// 		newstring[i] = s2[i - s1len];
+// 		i++;
+// 	}
+// 	free((void *) s2);
+// 	return (newstring);
+// }
 
 //searches the environment variables for
 //the OLDPWD. Returns the index if found, -1 if not
@@ -71,50 +71,51 @@ int	find_pwd(void)
 //updates OLDPWD and PWD environment variables according to
 //the new path given when using cd. If either OLDPWD or
 //PWD are unset does nothing
+//if PWD and OLDPWD still exist, both updated. if PWD unset, no longer updated but OLDPWD updated. If OLDPWD unset gets recreated
+void	change_pwd(int index)
+{
+	char	*newpath;
 
-void	update_pwd(void)
+	newpath = NULL;
+	if (index != -1)
+	{
+		newpath = getcwd(newpath, 0);
+		free(g_envp_copy[index]);
+		g_envp_copy[index] = ft_strjoin("PWD=", newpath);
+		free(newpath);
+	}
+}
+
+void	change_oldpwd(int index, char *home)
+{
+	char	*oldpwd;
+
+	oldpwd = NULL;
+	if (index != -1)
+	{
+		free(g_envp_copy[index]);
+		g_envp_copy[index] = ft_strjoin("OLDPWD=", home);
+	}
+	else
+	{
+		oldpwd = ft_strjoin("OLDPWD=", home);
+		addtoenv(oldpwd);
+		free(oldpwd);
+	}
+}
+
+void	update_pwd(char *home)
 {
 	int	index_pwd;
-	int index_oldpwd;
+	int	index_oldpwd;
 
 	index_pwd = find_pwd();
 	index_oldpwd = find_oldpwd();
-	if (index_pwd != 1)
-		change_pwd();
-	if (index_oldpwd != 1)
-		change_oldpwd();
+	change_pwd(index_pwd);
+	change_oldpwd(index_oldpwd, home);
+	if (home)
+		free(home);
 }
-// void	update_pwd(void)
-// {
-// 	int		index_pwd;
-// 	int		index_oldpwd;
-// 	char	*new_path;
-// 	char	**old_path;
-
-// 	new_path = NULL;
-// 	new_path = getcwd(new_path, 0);
-// 	index_oldpwd = find_oldpwd();
-// 	index_pwd = find_pwd();
-// 	if (index_pwd != -1)
-// 	{
-// 		old_path = ft_split(g_envp_copy[index_pwd], '=');
-// 		free(g_envp_copy[index_pwd]);
-// 		g_envp_copy[index_pwd] = ft_pwdjoinfree("PWD=", new_path);
-// 	}
-// 	if (index_oldpwd != -1 && index_pwd != -1)
-// 	{
-// 		if (old_path[1])
-// 		{
-// 			free(g_envp_copy[index_oldpwd]);
-// 			g_envp_copy[index_oldpwd] = ft_strjoin("OLDPWD=", old_path[1]);
-// 			free_the_pp(old_path);
-// 		}
-// 	}
-// 	else
-// 		free_the_pp(old_path);
-// 	if (index_pwd == -1)
-// 		free(new_path);
-// }
 
 //changes current directory based on argument
 //given. Also updates PWD and OLDPWD.
@@ -134,10 +135,11 @@ void	handle_cd(t_cmd *cmd)
 	}
 	else
 	{
+		home = getcwd(home, 0);
 		res = chdir(cmd->argv[1]);
 		set_exit_code(0);
 	}
-	update_pwd();
+	update_pwd(home);
 	if (res == -1)
 	{
 		dprintf(STDERR_FILENO, "minicougar: cd: \
