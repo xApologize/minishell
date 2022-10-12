5448929bfec34a1f5796b936632fec347147137c
fenix144 <yan_1731@hotmail.com>
2022-10-12 16:43:13 -0400

    Fix error with unclosed quotes

--- src/quotation.c
+++ src/quotation.c
@@ -4,35 +4,41 @@ void	error_quotation(t_data *data)
 {
 	data->error_quotes = false;
 	double_check(data);
-	if (data->error_quotes == false)
-		single_check(data);
 	if (data->error_quotes == true)
 		dprintf(STDERR_FILENO, "minicougar: odd number of quotes\n");
 }
 
-void	double_check(t_data *data)
-{
-	char *line;
 
-	line = data->line;
-}
-
+//if encounters single || double, set trigger to 1 and store character encountered. if character encountered again set trigger to 0.
+//keep going until end of string
 void	double_check(t_data *data)
 {
 	int	i;
 	int	trigger;
-
-	i = -1;
+	char c;
+	
+	i = 0;
+	c = 0;
 	trigger = 0;
-	while (data->line[++i])
+	while (data->line[i])
 	{
-		if (data->line[i] == '"' && trigger == 0)
+		if (data->line[i] == '\'' || data->line[i] == '"')
 		{
+			c = data->line[i];
 			trigger = 1;
 			i++;
+			while (data->line[i])
+			{
+				if (data->line[i] == c)
+				{
+					trigger = 0;
+					c = 0;
+					break;
+				}
+				i++;
+			}
 		}
-		if (data->line[i] == '"' && trigger == 1)
-			trigger = 0;
+		i++;
 	}
 	if (trigger == 1)
 		data->error_quotes = true;
@@ -43,17 +49,20 @@ void	single_check(t_data *data)
 	int	i;
 	int	trigger;
 
-	i = -1;
+	i = 0;
 	trigger = 0;
-	while (data->line[++i])
+	while (data->line[i])
 	{
 		if (data->line[i] == '\'' && trigger == 0)
 		{
 			trigger = 1;
 			i++;
+			while (data->line[i] != '\'' && data->line[i])
+				i++;
 		}
 		if (data->line[i] == '\'' && trigger == 1)
 			trigger = 0;
+		i++;
 	}
 	if (trigger == 1)
 		data->error_quotes = true;
