981682169886e337241465d1ca95ff5fe7ae8004
xApologize <jenbenrossignol@hotmail.com>
2022-10-14 15:57:16 -0400

    fix mem leak

--- src/search_cmds.c
+++ src/search_cmds.c
@@ -67,7 +67,7 @@ void	set_cmd(t_cmd *cmd, t_data *data)
 		cmd->cmd = handle_dollar(ft_strdup(line_cp));
 	}
 	else
-		cmd->cmd = get_path(handle_dollar(stripstring(ft_strdup(data->line))), data);
+		cmd->cmd = get_path(handle_dollar(stripstring(data->line)), data);
 	while (*line_cp != '\0')
 		line_cp++;
 	cmd->argv = get_argv(data);
