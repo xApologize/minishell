1818aeff9cbce654cdc818d72426aaa0fafce7c9
fenix144 <yan_1731@hotmail.com>
2022-10-17 16:15:30 -0400

    Fix compil

--- src/minishell.c
+++ src/minishell.c
@@ -1,6 +1,6 @@
 #include "../include/minishell.h"
 
-extern char	**g_envp_copy;
+char	**g_envp_copy;
 
 // need to fix prompt that shows up twice when using ctrl-c when signal is reset
 int	main(void)
